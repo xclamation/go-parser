f76f7b476fc72b76ee3baa0b17c1500192d66f2f
xclamation <gromov.ramil@crtex.io>
2025-03-15 17:07:32 +0300

    add tests

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.24.0
 require (
 	code.sajari.com/docconv/v2 v2.0.0-pre.4
 	github.com/PuerkitoBio/goquery v1.10.2
-	github.com/go-ole/go-ole v1.3.0
+	github.com/ozontech/allure-go/pkg/allure v0.6.14
+	github.com/ozontech/allure-go/pkg/framework v0.6.33
 )
 
 require (
@@ -13,20 +14,24 @@ require (
 	github.com/advancedlogic/GoOse v0.0.0-20191112112754-e742535969c1 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/araddon/dateparse v0.0.0-20200409225146-d820a6159ab1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/set v0.2.1 // indirect
 	github.com/gigawattio/window v0.0.0-20180317192513-0f5467e35573 // indirect
 	github.com/go-resty/resty/v2 v2.3.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/jaytaylor/html2text v0.0.0-20200412013138-3577fbdbcff7 // indirect
 	github.com/levigross/exp-html v0.0.0-20120902181939-8df60c69a8f5 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/olekukonko/tablewriter v0.0.4 // indirect
 	github.com/otiai10/gosseract/v2 v2.4.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/richardlehane/mscfb v1.0.3 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,6 +1,10 @@
 package parser
 
 import (
+	"github.com/ozontech/allure-go/pkg/framework/provider"
+	"github.com/ozontech/allure-go/pkg/framework/suite"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/xclamation/go-parser/pkg/consts"
 	"io"
 	"os"
@@ -12,95 +16,114 @@ const (
 	PATH string = "../../examples"
 )
 
-func TestHTMLParser(t *testing.T) {
+type ParserSuite struct {
+	suite.Suite
+}
+
+func TestParserSuite(t *testing.T) {
+	suite.RunSuite(t, new(ParserSuite))
+}
+
+func (s *ParserSuite) TestHTMLParser(t provider.T) {
+	t.Epic("Parser")
+	t.Feature("HTML Parsing")
+	t.Title("Test HTML Parser")
+	t.Description("This test checks if the HTML parser correctly parses an HTML file.")
+
 	HtmlParser := NewHTMLParser()
 	text, err := HtmlParser.Parse(filepath.Join(PATH, "example.html"))
-	if err != nil {
-		t.Fatalf("Failed to parse HTML file: %v", err)
-	}
 
-	if text != consts.ExpectedHTML {
-		t.Errorf("\nExpected:\n'%s'\ngot:\n'%s'", consts.ExpectedHTML, text)
-	}
+	// Проверка на ошибку
+	require.NoError(t, err, "Failed to parse HTML file")
+
+	// Проверка результата
+	assert.Equal(t, consts.ExpectedHTML, text, "Parsed HTML does not match expected")
 }
 
-func TestDOCXParser(t *testing.T) {
+func (s *ParserSuite) TestDOCXParser(t provider.T) {
+	t.Epic("Parser")
+	t.Feature("DOCX Parsing")
+	t.Title("Test DOCX Parser")
+	t.Description("This test checks if the DOCX parser correctly parses a DOCX file.")
+
 	DOCXParser := NewDOCXParser()
 	text, err := DOCXParser.Parse(filepath.Join(PATH, "example.docx"))
-	if err != nil {
-		t.Fatalf("Failed to parse DOCX file: %v", err)
-	}
 
+	// Проверка на ошибку
+	require.NoError(t, err, "Failed to parse DOCX file")
+
+	// Чтение ожидаемого результата из файла
 	file, err := os.Open(filepath.Join(PATH, "docx.txt"))
-	if err != nil {
-		t.Fatalf("Failed to open file: %v", err)
-	}
-	defer file.Close() // Закрываем файл после завершения
+	require.NoError(t, err, "Failed to open file")
+	defer file.Close()
 
 	expectedDOCX, err := io.ReadAll(file)
-	if err != nil {
-		t.Fatalf("Failed to read file: %v", err)
-	}
+	require.NoError(t, err, "Failed to read file")
 
-	if text != string(expectedDOCX) {
-		t.Errorf("\nExpected:\n'%s'\ngot:\n'%s'", expectedDOCX, text)
-	}
+	// Проверка результата
+	assert.Equal(t, string(expectedDOCX), text, "Parsed DOCX does not match expected")
 }
 
-func TestDOCParser(t *testing.T) {
+func (s *ParserSuite) TestDOCParser(t provider.T) {
+	t.Epic("Parser")
+	t.Feature("DOC Parsing")
+	t.Title("Test DOC Parser")
+	t.Description("This test checks if the DOC parser correctly parses a DOC file.")
+
 	DOCParser := NewDOCParser()
 	text, err := DOCParser.Parse(filepath.Join(PATH, "example.doc"))
-	if err != nil {
-		t.Fatalf("Failed to parse DOC file: %v", err)
-	}
 
-	if text != consts.ExpectedDOC {
-		t.Errorf("\nExpected:\n'%s'\ngot:\n'%s'", consts.ExpectedDOC, text)
-	}
+	// Проверка на ошибку
+	require.NoError(t, err, "Failed to parse DOC file")
+
+	// Проверка результата
+	assert.Equal(t, consts.ExpectedDOC, text, "Parsed DOC does not match expected")
 }
 
-func TestPDFParser(t *testing.T) {
+func (s *ParserSuite) TestPDFParser(t provider.T) {
+	t.Epic("Parser")
+	t.Feature("PDF Parsing")
+	t.Title("Test PDF Parser")
+	t.Description("This test checks if the PDF parser correctly parses a PDF file.")
+
 	PDFParser := NewPDFParser()
 	text, err := PDFParser.Parse(filepath.Join(PATH, "example.pdf"))
-	if err != nil {
-		t.Fatalf("Failed to parse PDF file: %v", err)
-	}
 
+	// Проверка на ошибку
+	require.NoError(t, err, "Failed to parse PDF file")
+
+	// Чтение ожидаемого результата из файла
 	file, err := os.Open(filepath.Join(PATH, "pdf.txt"))
-	if err != nil {
-		t.Fatalf("Failed to open file: %v", err)
-	}
+	require.NoError(t, err, "Failed to open file")
 	defer file.Close()
 
 	expectedPDF, err := io.ReadAll(file)
-	if err != nil {
-		t.Fatalf("Failed to read file: %v", err)
-	}
+	require.NoError(t, err, "Failed to read file")
 
-	if text != string(expectedPDF) {
-		t.Errorf("\nExpected:\n'%s'\ngot:\n'%s'", expectedPDF, text)
-	}
+	// Проверка результата
+	assert.Equal(t, string(expectedPDF), text, "Parsed PDF does not match expected")
 }
 
-func TestDJVUParser(t *testing.T) {
+func (s *ParserSuite) TestDJVUParser(t provider.T) {
+	t.Epic("Parser")
+	t.Feature("DJVU Parsing")
+	t.Title("Test DJVU Parser")
+	t.Description("This test checks if the DJVU parser correctly parses a DJVU file.")
+
 	DJVUParser := NewDJVUParser()
 	text, err := DJVUParser.Parse(filepath.Join(PATH, "example.djvu"))
-	if err != nil {
-		t.Fatalf("Failed to parse DJVU file: %v", err)
-	}
 
+	// Проверка на ошибку
+	require.NoError(t, err, "Failed to parse DJVU file")
+
+	// Чтение ожидаемого результата из файла
 	file, err := os.Open(filepath.Join(PATH, "djvu.txt"))
-	if err != nil {
-		t.Fatalf("Failed to open file: %v", err)
-	}
+	require.NoError(t, err, "Failed to open file")
 	defer file.Close()
 
 	expectedDJVU, err := io.ReadAll(file)
-	if err != nil {
-		t.Fatalf("Failed to read file: %v", err)
-	}
+	require.NoError(t, err, "Failed to read file")
 
-	if text != string(expectedDJVU) {
-		t.Errorf("\nExpected:\n'%s'\ngot:\n'%s'", expectedDJVU, text)
-	}
+	// Проверка результата
+	assert.Equal(t, string(expectedDJVU), text, "Parsed DJVU does not match expected")
 }
